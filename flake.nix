aafbb43fe8d680e77841e7dabd1a7a83c98200ab
a-kanev@yandex.ru <a-kanev@yandex.ru>
2024-03-11 15:36:04 +0600

    Init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  outputs = inputs @ {self, ...}:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [
+        inputs.nixos-flake.flakeModule
+      ];
+
+      flake.nixosConfigurations.nixorn = self.nixos-flake.lib.mkLinuxSystem {
+        imports = [ 
+          ./nixos/configuration.nix
+        ];
+      };
+    };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixos-flake.url = "github:srid/nixos-flake";
+  };
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,160 @@
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Bishkek";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ru_RU.UTF-8";
+    LC_MEASUREMENT = "ru_RU.UTF-8";
+    LC_MONETARY = "ru_RU.UTF-8";
+    LC_NAME = "ru_RU.UTF-8";
+    LC_NUMERIC = "ru_RU.UTF-8";
+    LC_PAPER = "ru_RU.UTF-8";
+    LC_TELEPHONE = "ru_RU.UTF-8";
+    LC_TIME = "ru_RU.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nixorn = {
+    isNormalUser = true;
+    description = "nixorn";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+  home-manager.users.nixorn = { pkgs, ... }: {
+     home.packages = [
+	pkgs.gimp
+	pkgs.inkscape
+	pkgs.libreoffice
+	pkgs.telegram-desktop
+	pkgs.ranger
+        pkgs.firefox
+        pkgs.thunderbird
+        pkgs.keepass
+        pkgs.discord
+        pkgs.python311Packages.pip
+        pkgs.ncdu
+        pkgs.curl
+        pkgs.vesktop
+        ];
+	programs.htop.enable = true;
+	programs.vscode.enable = true;
+	programs.tmux.enable = true;
+	programs.vim.enable = true;
+	programs.git = {
+		enable = true;
+		userName = "a-kanev@yandex.ru";
+		userEmail = "a-kanev@yandex.ru";
+              };
+        programs.direnv = {
+          enable = true;
+          nix-direnv.enable = true;
+        };
+        programs.bash.enable = true;
+
+        home.stateVersion = "23.11";
+	nixpkgs.config.allowUnfree = true;
+  };
+
+  # Allow unfree packages
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  services.blueman.enable = true;
+}
