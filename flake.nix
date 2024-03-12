f1cca3932e6d7f186bbff561487c9ed1f9bf8c18
a-kanev@yandex.ru <a-kanev@yandex.ru>
2024-03-12 20:32:23 +0600

    NH

--- flake.nix
+++ flake.nix
@@ -3,19 +3,26 @@
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux"];
       imports = [
+        
         inputs.nixos-flake.flakeModule
       ];
 
-      flake.nixosConfigurations.nixorn = self.nixos-flake.lib.mkLinuxSystem {
+
+      flake.nixosConfigurations.nixorn-legion = self.nixos-flake.lib.mkLinuxSystem {
         imports = [ 
           inputs.nixos-hardware.nixosModules.lenovo-legion-15arh05h
           inputs.home-manager.nixosModules.home-manager
+          inputs.nh.nixosModules.default
           ./nixos/configuration.nix
         ];
       };
     };
 
   inputs = {
+    nh = {
+      url = "github:viperML/nh";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixos-flake.url = "github:srid/nixos-flake";
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -13,12 +13,7 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.hostName = "nixorn-legion"; # Define your hostname.
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -67,16 +62,8 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.nixorn = {
@@ -102,6 +89,7 @@ in
         vesktop
         python311Packages.plantuml-markdown
         python311Packages.mkdocs
+        nom
         ];
       programs.firefox.enable = true;
       
@@ -142,41 +130,10 @@ in
   nixpkgs.config.allowUnfree = true;
 
 
-  # Allow unfree packages
+  environment.systemPackages = with pkgs; [ ];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11";
 
   systemd.oomd = {
     enable = true;
@@ -185,6 +142,16 @@ in
   };
 
   zramSwap.enable = true;
+
+  # хелпер для запуска всего
+  nh = {
+    enable = true;
+    clean = {
+      enable = true;
+      extraArgs = "--keep-since 30d --keep 5";
+    };
+  };
+  environment.sessionVariables.FLAKE = "/home/nixorn/system"; # Чтобы работал nh
   
   nix = {
     settings = {
